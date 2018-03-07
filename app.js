07e47226d4963b8eac5b39606f1eeee8ae2d6c8b
IgnorePT <n3ls6n@hotmail.com>
2018-03-07 09:46:21 +0000

    Add core functions script

--- app.js
+++ app.js
@@ -3,26 +3,11 @@ const yargs = require('yargs');
 const axios = require('axios');
 const hbs = require('hbs');
 
+const core = require('./core/core');
+
 const port = process.env.PORT || 3000;
 
 var url = "http://s.webservices.yugoup.com/Service.svc/getSettings";
-
-var paramsForRequest = {
-    publicKey: "yugoup1",
-    platformId: 2,
-    lang: "pt-PT",
-    userId: "00000000-0000-0000-0000-000000000000",
-    IdDeviceOS: 2,
-    uuid: "00000000000000000001"};
-
-var reqParams = JSON.stringify(paramsForRequest);
-
-var myBuffer = [];
-var str = 'Stack Overflow';
-var buffer = new Buffer(reqParams, 'utf8');
-for (var i = 0; i < buffer.length; i++) {
-    myBuffer.push(buffer[i]);
-}
 
 var app = express();
 
@@ -39,14 +24,14 @@ app.get('/',(req, res) => {
             "content-type": "application/octet-stream",
             "Accept": 'application/json'
         },
-        data: paramsForRequest
+        data: core.paramsForRequest
     }).then((response) => {
     
         //res.send(JSON.stringify(response.data));
-        //console.log(JSON.stringify(response.data,undefined,2));
         res.render('index.hbs', {
-            pageTitle: 'Company Demo',
-            welcomeMessage: 'Bemvindo ao website isto esta a ficar porreiro'
+            pageTitle: response.data.settings.settings.name,
+            smallDescription: response.data.settings.settings.smallDescription,
+            companyImage: response.data.settings.settings.aboutImage
         });
     
     }).catch((err) => {
---
+++ core/core.js
@@ -0,0 +1,13 @@
+
+
+
+var paramsForRequest = {
+    publicKey: "yugoup1",
+    platformId: 2,
+    lang: "pt-PT",
+    userId: "00000000-0000-0000-0000-000000000000",
+    IdDeviceOS: 2,
+    uuid: "00000000000000000001"};
+
+
+    module.exports = {paramsForRequest};
